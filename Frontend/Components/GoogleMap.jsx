7ebd7953852adfda5c9e35993e35f78f208d174e
John <johnminton04@gmail.com>
2024-06-25 11:32:09 +1200

    added custom entry for locations

--- Frontend/Components/GoogleMap.jsx
+++ Frontend/Components/GoogleMap.jsx
@@ -22,7 +22,7 @@ const GoogleMap = () => {
     const directionsRenderer = new window.google.maps.DirectionsRenderer();
     const map = new window.google.maps.Map(document.getElementById('map'), {
       zoom: 7,
-      center: { lat: 41.85, lng: -87.65 },
+      center: { lat: -36.880184, lng: 174.754739 },
     });
     directionsRenderer.setMap(map);
     directionsRenderer.setPanel(document.getElementById('directionsPanel'));
@@ -56,35 +56,9 @@ const GoogleMap = () => {
     <div>
       <div>
         <b>Start:</b>
-        <select id="start">
-          <option value="Auckland, NZ">Auckland</option>
-          <option value="St Louis, MO">St Louis</option>
-          <option value="Joplin, MO">Joplin, MO</option>
-          <option value="Oklahoma City, OK">Oklahoma City</option>
-          <option value="Amarillo, TX">Amarillo</option>
-          <option value="Gallup, NM">Gallup, NM</option>
-          <option value="Flagstaff, AZ">Flagstaff, AZ</option>
-          <option value="Winona, AZ">Winona</option>
-          <option value="Kingman, AZ">Kingman</option>
-          <option value="Barstow, CA">Barstow</option>
-          <option value="San Bernardino, CA">San Bernardino</option>
-          <option value="Los Angeles, CA">Los Angeles</option>
-        </select>
+        <input type="text" id="start" placeholder="Enter start location" />
         <b>End:</b>
-        <select id="end">
-          <option value="Wellington, NZ">Wellington, NZ</option>
-          <option value="San Bernardino, CA">San Bernardino</option>
-          <option value="Barstow, CA">Barstow</option>
-          <option value="Kingman, AZ">Kingman</option>
-          <option value="Winona, AZ">Winona</option>
-          <option value="Flagstaff, AZ">Flagstaff</option>
-          <option value="Gallup, NM">Gallup</option>
-          <option value="Amarillo, TX">Amarillo</option>
-          <option value="Oklahoma City, OK">Oklahoma City</option>
-          <option value="Joplin, MO">Joplin, MO</option>
-          <option value="St Louis, MO">St Louis</option>
-          <option value="Chicago, IL">Chicago</option>
-        </select>
+        <input type="text" id="end" placeholder="Enter end location" />
       </div>
       <div id="map" style={{ height: '500px', width: '100%' }}></div>
       <div id="directionsPanel" style={{ height: '500px', width: '100%' }}></div>
