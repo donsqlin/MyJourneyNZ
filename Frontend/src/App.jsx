03bb696621f1676cb4b798ac5414bf643d6907c4
John <johnminton04@gmail.com>
2024-06-25 15:57:09 +1200

    cleaned

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,26 +19,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* GOOGLE MAPS */}
+
+      {/* Input Entries to be sent to Google Map API */}
       <div>
         <b>Start:</b>
         <input
@@ -66,6 +50,7 @@ function App() {
         </select>
         <button onClick={handleCalculateRoute}>Calculate Route</button>
       </div>
+
       <GoogleMap ref={googleMapRef} start={start} end={end} travelMode={travelMode} />
     </>
   );
