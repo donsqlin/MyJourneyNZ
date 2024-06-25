21dfab4361d9422889a331c03b84c14047e8e419
Matthew N <mattnanthameechai@gmail.com>
2024-06-25 15:41:22 +1200

    Created Nav Bar

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Map from '../Map/Map.jsx'
 import Modal from '../Modal/Modal.jsx'
-
+import NavBar from '../NavBar/NavBar.jsx'
 
 const Home = () => {
   return (
     <>
-        <div>
-            <h1 className='text-center py-5 bg-lime-500'>Events</h1>
-        </div>
-        <Map />
-        <Modal />
+      {/* <div>
+        <h1 className="text-center py-5 bg-lime-500">Events</h1>
+      </div> */}
+      <NavBar />
+      <Map />
+      <Modal />
     </>
   )
 }
---
+++ Frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,48 @@
+function NavBar() {
+  return (
+    <nav className="bg-[#3479C2] flex justify-between text-white ">
+      <div className="max-w-7xl px-2 sm:px-6 lg:px-8 flex items-center content-center h-16 gap-2">
+        <div className="relative flex h-16 items-center">
+          <button className="w-5 h-5 rounded-full bg-white text-center">
+            <p className="text-[#3479C2] font-bold	text-2xl mt-[-8px]">&lt;</p>
+          </button>
+        </div>
+
+        <div className=" flex gap-2 ">
+          <img
+            src="../src/assets/bus-white.png"
+            alt="car"
+            className="max-w-6"
+          />
+          <img
+            src="../src/assets/train-white.png"
+            alt="car"
+            className="max-w-6"
+          />
+        </div>
+      </div>
+
+      <div className="max-w-7xl px-2 sm:px-6 lg:px-8 h-16 gap-2 content-center ">
+        <div className="flex items-center gap-1">
+          <img
+            src="../src/assets/finish-flag-white.png"
+            alt="car"
+            className="max-w-3"
+          />
+          <div>Destination</div>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <img
+            src="../src/assets/location-white.png"
+            alt="car"
+            className="max-w-3"
+          />
+          <div>Current Location</div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
