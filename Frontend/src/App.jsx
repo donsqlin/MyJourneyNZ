d090b19d633056cbc4f796596c624a0980537568
Matthew <mnanthameechai@gmail.com>
2024-06-26 04:37:00 +0000

    Resolve "Break up div directionsPanel into data and do not display"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useRef } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Home from './components/Home/Home.jsx'
 
 function App() {
