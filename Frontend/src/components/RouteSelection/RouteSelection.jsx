accebe0ea54e96570f0d0551dca71e0bd31ca005
alden maiyor <amai111@aucklanduni.ac.nz>
2024-06-26 16:08:36 +1200

    added buttons for separate routes

--- Frontend/src/components/RouteSelection/RouteSelection.jsx
+++ Frontend/src/components/RouteSelection/RouteSelection.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
+import RouteButton from '../fancyButtons/RouteButton'
+import OtherRouterButton from '../fancyButtons/OtherRouterButtons.jsx'
+import AtRouterButton from '../fancyButtons/AtButton.jsx'
+import Beam1Button from '../fancyButtons/Beam1Button.jsx'
+import Beam2Button from '../fancyButtons/Beam2Button.jsx'
+import UberButton from '../fancyButtons/UberButton.jsx'
 
 const RouteSelection = ({ sortby }) => {
 
 
 
     return (
-        <div>
-            <div>
-                <h1 className='font-bold'>{sortby}</h1>
+        <div >
+            <div className=''>
+                <h1 className='font-bold text-center'>{sortby}</h1>
             </div>
 
             <div>
-                <h1 className='font-bold mt-[20px]'>Top Suggested Route</h1>
-                <div className='border-2 mt-[20px]'>
-                    
+                <h1 className='font-bold mt-[20px] text-center'>Top Suggested Route</h1>
+                <div>
+                    <RouteButton />
                 </div>
             </div>
 
-            <div>
-                <h1 className='font-bold mt-[20px]'>Other Journeys</h1>
-                <div>
-                    
+            <div className='flex flex-col justify-center'>
+                <h1 className='font-bold mt-[20px] text-center'>Other Journeys</h1>
+                <div className='max-h-[300px] overflow-y-auto'>
+
+                    <div>
+                        <AtRouterButton />
+                    </div>
+                    <div>
+                        <Beam1Button />
+                    </div>
+                    <div>
+                        <Beam2Button />
+                    </div>
+                    <div>
+                        <UberButton />
+                    </div>
                 </div>
             </div>
         </div>
---
+++ Frontend/src/components/fancyButtons/UberButton.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Box, Typography } from '@mui/material';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import DirectionsSubwayIcon from '@mui/icons-material/DirectionsSubway';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { styled } from '@mui/system';
+import Atlogo from "../../assets/at-logo.png"
+import UberLogo from "../../assets/Uber.svg"
+
+const DottedLine = styled('div')({
+    flexGrow: 1,
+    height: '2px',
+    borderTop: '2px dotted black',
+    margin: '0 5px',
+});
+
+const UberButton = () => {
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                backgroundColor: 'white',
+                color: 'black',
+                borderRadius: '10px',
+                boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+                padding: '10px',
+                textAlign: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: '100%',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <img src={UberLogo} className="w-[50px] h-[40px]"></img>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <h3> UBERX/UBER GREEN</h3>
+                    {/* <DirectionsBusIcon sx={{ color: 'black' }} />
+                    <DottedLine />
+                    <DirectionsSubwayIcon sx={{ color: 'black' }} /> */}
+                </Box>
+                <Box sx={{display:"flex", flexDirection:"column"}}>
+                    <Typography sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+                        $47.50
+                    </Typography>
+                    <Typography sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+                        30 MINS
+                    </Typography>
+                </Box>
+
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    width: '100%',
+                    marginTop: '10px',
+                }}
+            >
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <ScheduleIcon sx={{ color: '#fff', marginRight: '5px' }} />
+                    <Typography sx={{fontSize: "10px"}}>WEST leaves 4:09pm from Parnell Train station</Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default UberButton;
