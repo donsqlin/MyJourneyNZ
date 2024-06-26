55275270f3ab840c4ed9863794510d15546a5b42
John <johnminton04@gmail.com>
2024-06-26 16:53:21 +1200

    tidied up pre integration

--- Frontend/src/components/fancyButtons/AtButton.jsx
+++ Frontend/src/components/fancyButtons/AtButton.jsx
@@ -3,9 +3,8 @@ import { Box, Typography } from '@mui/material';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import DirectionsSubwayIcon from '@mui/icons-material/DirectionsSubway';
 import ScheduleIcon from '@mui/icons-material/Schedule';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { styled } from '@mui/system';
-import Atlogo from "../../assets/AT.svg"
+import Atlogo from "../../assets/AT.svg";
 
 const DottedLine = styled('div')({
     flexGrow: 1,
@@ -15,6 +14,10 @@ const DottedLine = styled('div')({
 });
 
 const AtRouterButton = () => {
+    const handleClick = () => {
+        console.log('AtRouterButton clicked');
+    };
+
     return (
         <Box
             sx={{
@@ -28,7 +31,9 @@ const AtRouterButton = () => {
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
+                cursor: 'pointer', // Add cursor style to indicate clickability
             }}
+            onClick={handleClick}
         >
             <Box
                 sx={{
@@ -38,22 +43,21 @@ const AtRouterButton = () => {
                     justifyContent: 'space-between',
                 }}
             >
-                <img src={Atlogo} className="w-[50px] h-[40px]"></img>
+                <img src={Atlogo} alt="AT logo" className="w-[50px] h-[40px]" />
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <h3> BUS</h3>
+                    <Typography variant="body2">BUS</Typography>
                     <DirectionsBusIcon sx={{ color: 'black' }} />
                     <DottedLine />
                     <DirectionsSubwayIcon sx={{ color: 'black' }} />
                 </Box>
-                <Box sx={{display:"flex", flexDirection:"column"}}>
+                <Box sx={{ display:"flex", flexDirection:"column" }}>
                     <Typography sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
                         <span className="line-through">$4.35</span> FREE
                     </Typography>
                     <Typography sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
-                        53 MIN +delays
+                        53 MIN + delays
                     </Typography>
                 </Box>
-
             </Box>
             <Box
                 sx={{
@@ -65,7 +69,7 @@ const AtRouterButton = () => {
                 }}
             >
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <ScheduleIcon sx={{ color: '#fff', marginRight: '5px' }} />
+                    <ScheduleIcon sx={{ color: 'black', marginRight: '5px' }} />
                     <Typography>WEST leaves 4:09pm from Parnell Train station</Typography>
                 </Box>
             </Box>
